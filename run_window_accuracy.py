7969f42fb8d25cf3e5a1822fef8c02f5303eea02
Jiatu Liu <jiatu@opendoor.com>
2018-12-12 22:44:45 -0800

    Use GPU, other cleanups

--- run_window_accuracy.py
+++ run_window_accuracy.py
@@ -1,6 +1,10 @@
+import sys
+
+import torch
+from torch.utils.data import DataLoader
+
 from datasets.kaggle_dataset_modified import KaggleTestDatasetModified
 from extractors.word2vec_2d_extractor import Word2Vec2DExtractor
-from torch.utils.data import DataLoader
 from extractors.window_accuracy import WindowAccuracy
 
 TRAIN_DATA_PATH = 'data/train.csv'
@@ -8,19 +12,22 @@ TEST_DATA_PATH = 'data/processed.csv'
 TEST_LABEL_PATH = 'data/test_labels.csv'
 
 BATCH_SIZE = 25
-DATA_LOADER_NUM_WORKERS = 2
+DATA_LOADER_NUM_WORKERS = 5
 
-def run_window_accuracy():
+
+def run_window_accuracy(model_path: str):
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
     test_data = _test_data()
-    path = "cnn_2018-12-12T21:32:47.472484.model"
-    window_accuracy = WindowAccuracy(model_path=path, loader=test_data)
+    window_accuracy = WindowAccuracy(model_path=model_path, loader=test_data, device=device)
     window_accuracy.check_window_accuracy('outputs/test_window_two.txt')
 
 
 def _test_data():
     word2vec_extractor = Word2Vec2DExtractor(train_data_path=TRAIN_DATA_PATH)
     test_dataset = KaggleTestDatasetModified(TEST_DATA_PATH, TEST_LABEL_PATH, word2vec_extractor)
-    # Reserve 10% of data for validation purposes.
     loader_test = DataLoader(
         test_dataset,
         batch_size=BATCH_SIZE,
@@ -30,4 +37,6 @@ def _test_data():
     return loader_test
 
 
-run_window_accuracy()
+if __name__ == '__main__':
+    saved_model_path = f'saved_models/{sys.argv[1]}'
+    run_window_accuracy(saved_model_path)
